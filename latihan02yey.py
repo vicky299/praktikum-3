fb60be7cd5e0006d6400fbd802ce5b737afee988
vicky299 <71803020+vicky299@users.noreply.github.com>
2020-11-22 19:46:36 -0800

    Initial commit

---
+++ latihan02yey.py
@@ -0,0 +1,6 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: C:/pythonProject/lat 2.py ======================
+hello world hello world hello world 
+>>> 
